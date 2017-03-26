6e619217880a517a10054d229844cbc470835793
azertyfun <azertyfun_poubelle@hotmail.fr>
2017-03-26 17:06:26 +0200

    Updated build environment and switched to OpenWeatherMap Forecast.io is just plain inacurrate for me

--- src/pkjs/index.js
+++ src/pkjs/index.js
@@ -5,104 +5,114 @@ var current_settings;
 
 
 // converts forecast.io weather icon code to Yahoo weather icon code (to reuse current bitmap with icon set)
-  var ForecastIoIconToYahooIcon = function(forecsat_io_icon) {
+  var OWMIconToYahooIcon = function(owm_icon) {
     var yahoo_icon = 3200; //initialy not defined
-    
-    switch (forecsat_io_icon){
-      case "clear-day":
+
+    switch (owm_icon) {
+      case "01d":
         yahoo_icon = 32; // sunny
         break;
-      case "clear-night":
+      case "01n":
         yahoo_icon = 31; // clear night
         break;
-      case "rain":
-        yahoo_icon = 11; // showers
-        break;
-      case "snow":
-        yahoo_icon = 16; // snow
-        break;
-      case "sleet": 
-        yahoo_icon = 18; // sleet
-        break;
-      case "wind": 
-        yahoo_icon = 24; // windy
-        break;
-      case "fog": 
-        yahoo_icon = 20; // foggy
-        break;
-      case "cloudy":
-        yahoo_icon = 26; // cloudy
-        break;
-      case "partly-cloudy-day":
+      case "02d":
         yahoo_icon = 30; // partly cloudy day
         break;
-      case "partly-cloudy-night":
+      case "02n":
         yahoo_icon = 29; // partly cloudy night
         break;
+      case "03d":
+      case "03n":
+      case "04d":
+      case "04n":
+        yahoo_icon = 26; // cloudy
+        break;
+      case "09d":
+      case "09n":
+      case "10d":
+      case "10n":
+        yahoo_icon = 11; // showers
+        break;
+      case "11d":
+      case "11n":
+        yahoo_icon = 4; // thunderstorm
+        break;
+      case "13d":
+      case "13n":
+        yahoo_icon = 16; // snow
+        break;
+      case "50d":
+      case "50n":
+        yahoo_icon = 20; // foggy
+        break;
     }
-    
+
     return yahoo_icon;
-    
+
   };
 
+function fahrenheit(temperature) {
+    return 1.8 * (temperature - 273.15) + 32;
+}
 
-//2016-03-25: Updated for Forecast.io
-function getWeather(coords /*woeid*/ ) {  
-  
-      if (current_settings.forecastIoApiKey === '') {
-        //console.log ("\n++++ I am inside of 'getWeather()' API KEY NOT DEFINED");
-        return;
-      }
-  
+function celcius(temperature) {
+    return temperature - 273.15;
+}
+
+//2017-03-26: Updated for OpenWeatherMap
+function getWeather(lat, lon /*woeid*/ ) {
+
+  if (current_settings.OWMApiKey === '') {
+    console.log ("\n++++ I am inside of 'getWeather()' API KEY NOT DEFINED");
+    return;
+  }
+
   var temperature;
   var icon;
-  
-  
-  //* var query = 'select item.condition from weather.forecast where woeid =  ' + woeid + ' and u="' + (current_settings.temperatureFormat === 0? 'f' : 'c') + '"';
-  //console.log ("++++ I am inside of 'getWeather()' preparing query:" + query);
-  
-  //* var url = 'https://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent(query) + '&format=json&env=store://datatables.org/alltableswithkeys';
-  var url = 'https://api.forecast.io/forecast/' + current_settings.forecastIoApiKey + '/' + coords + '?exclude=minutely,hourly,daily,alerts,flags&units=' + (current_settings.temperatureFormat === 0? 'us' : 'si');
-  //console.log ("++++ I am inside of 'getWeather()' preparing url:" + url);
-  
-  // ** Send request to Yahoo
-  //Send request to Forecast.io
+
+  var url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&APPID=' + current_settings.OWMApiKey;
+  console.log ("++++ I am inside of 'getWeather()' preparing url:" + url);
+
+  current_settings.temperatureFormat === 0? 'us' : 'si'
+
+  //Send request to OpenWeatherMap
   var xhr = new XMLHttpRequest();
   xhr.onload = function () {
-    
-    //console.log  ("++++ I am inside of 'getWeather()' callback. responseText is " + this.responseText);
-    
+
+    console.log  ("++++ I am inside of 'getWeather()' callback. responseText is " + this.responseText);
+
     var json = JSON.parse(this.responseText);
-    
-    
-    temperature = json.currently.temperature;
+
+
+    temperature = json.main.temp;
+    temperature = current_settings.temperatureFormat === 0 ? fahrenheit(temperature) : celcius(temperature);
     console.log  ("++++ I am inside of 'getWeather()' callback. Temperature is " + temperature);
-    
-    icon = json.currently.icon;
+
+    icon = json.weather[0].icon;
     console.log  ("++++ I am inside of 'getWeather()' callback. Icon code: " + icon);
-   
-    
+
+
     var dictionary = {
-      'KEY_WEATHER_CODE': ForecastIoIconToYahooIcon(icon),
+      'KEY_WEATHER_CODE': OWMIconToYahooIcon(icon),
       'KEY_WEATHER_TEMP': temperature
     };
-    
+
     // Send to Pebble
-    //console.log  ("++++ I am inside of 'getWeather()' callback. About to send message to Pebble");
+    console.log  ("++++ I am inside of 'getWeather()' callback. About to send message to Pebble");
     Pebble.sendAppMessage(dictionary,
     function(e) {
-      //console.log ("++++ I am inside of 'Pebble.sendAppMessage()' callback. Weather info sent to Pebble successfully!");
+      console.log ("++++ I am inside of 'Pebble.sendAppMessage()' callback. Weather info sent to Pebble successfully!");
     },
     function(e) {
-      //console.log ("++++ I am inside of 'Pebble.sendAppMessage()' callback. Error sending weather info to Pebble!");
+      console.log ("++++ I am inside of 'Pebble.sendAppMessage()' callback. Error sending weather info to Pebble!");
     }
     );
   };
-  
+
   xhr.onerror = function(e) {
-    //console.log("I am inside of 'getWeather()' ERROR: " + e.error);
+    console.log("I am inside of 'getWeather()' ERROR: " + e.error);
   };
-  
+
   xhr.open('GET', url);
   xhr.send();
 }
@@ -111,43 +121,15 @@ function getWeather(coords /*woeid*/ ) {
 
 // on location success querying woeid and getting weather
 function locationSuccess(pos) {
-//         // We neeed to get the Yahoo woeid first
-//         var woeid;
-      
-//       /* YG 2016-01-25  !!! This query no longer works due to Yahoo bug. Using the one below it !!!  */  
-//       // var query = 'select * from geo.placefinder where text="' +
-//       //     pos.coords.latitude + ',' + pos.coords.longitude + '" and gflags="R"';
-//          var query = 'select locality1 from geo.places where text="(' + 
-//              pos.coords.latitude + ',' + pos.coords.longitude + ')" limit 1';
-        
-//         //console.log ("++++ I am inside of 'locationSuccess()' preparing query:" + query);
-//         var url = 'https://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent(query) + '&format=json';
-//         //console.log ("++++ I am inside of 'locationSuccess()' preparing URL: " + url);
-//         // Send request to Yahoo
-//         var xhr = new XMLHttpRequest();
-//         xhr.onload = function () {
-//           var json = JSON.parse(this.responseText);
-          
-//           /* YG 2016-01-25  !!! This result no longer works due to Yahoo bug. Using the one below it !!!  */  
-//           // woeid = json.query.results.Result.woeid;
-//           woeid = json.query.results.place.locality1.woeid;
-          
-//           //console.log ("++++ I am inside of 'locationSuccess()', woeid received:" + woeid);
-//           getWeather(woeid);
-//         };
-//         xhr.open('GET', url);
-//         xhr.send();
-  
-   // requestion weather from forecast.io
-   getWeather(pos.coords.latitude + ',' + pos.coords.longitude);
-
+    console.log('Location success: ' + pos);
+   getWeather(pos.coords.latitude, pos.coords.longitude);
 }
 
 
 
 
 function locationError(err) {
-  //console.log ("++++ I am inside of 'locationError: Error requesting location!");
+  console.log ("++++ I am inside of 'locationError: Error requesting location!");
 }
 
 
@@ -162,16 +144,17 @@ function getLocation() {
 
 
 // Listen for when the watchface is opened
-Pebble.addEventListener('ready', 
+Pebble.addEventListener('ready',
   function(e) {
-      
+
     //reading current stored settings
     try {
        current_settings = JSON.parse(localStorage.getItem('current_settings'));
     } catch(ex) {
-       current_settings = null; 
-    }  
-    
+       current_settings = null;
+       console.log('Error: could not parse JSON');
+    }
+
      if (current_settings === null) {
          current_settings = {
              temperatureFormat: 0,
@@ -182,10 +165,12 @@ Pebble.addEventListener('ready',
              locationService: 0,
              woeid: version >= '2.22'? '' : 0,
              language: 255,
-             forecastIoApiKey: ''
+             OWMApiKey: ''
          };
+     } else if(current_settings.OWMApiKey === undefined) { // In case the old version of C&S was installed
+        current_settings.OWMApiKey = '';
      }
-    
+
     //console.log ("++++ I am inside of 'Pebble.addEventListener('ready'): PebbleKit JS ready!");
     var dictionary = {
         "KEY_JSREADY": 1
@@ -200,7 +185,7 @@ Pebble.addEventListener('ready',
       function(e) {
         //console.log ("++++ I am inside of 'Pebble.sendAppMessage() callback: Error ready notice to Pebble!");
       }
-    ); 
+    );
   }
 );
 
@@ -208,7 +193,7 @@ Pebble.addEventListener('ready',
 Pebble.addEventListener('appmessage',
   function(e) {
     console.log ("++++ I am inside of 'Pebble.addEventListener('appmessage'): AppMessage received");
-    
+
     if (current_settings.locationService == 1) { // for manual location - request weather right away
      //***** console.log ("\n++++ I am inside of 'Pebble.addEventListener('appmessage'): Requesting weather by WOEID");
      // console.log ("\n++++ I am inside of 'Pebble.addEventListener('appmessage'): Requesting weather by coords:" + current_settings.woeid);
@@ -217,33 +202,33 @@ Pebble.addEventListener('appmessage',
        console.log ("++++ I am inside of 'Pebble.addEventListener('appmessage'): Requesting automatic location");
        getLocation();  // for automatic location - get location
     }
-    
-  }                     
+
+  }
 );
 
-/*    ******************************************************************** Config Section ****************************************** */ 
+/*    ******************************************************************** Config Section ****************************************** */
 
 Pebble.addEventListener("showConfiguration",
   function(e) {
-   
+
     //Load the remote config page
-   
+
     //Pebble.openURL("http://codecorner.galanter.net/pebble/clean_smart_config.htm?version=" + version);
-    Pebble.openURL("http://ygalanter.github.io/configs/clean_smart/clean_smart_config.htm?version=" + version); //YG 2016-03-15; moved config to github hosting
-    
+    Pebble.openURL("http://azertyfun.github.io/configs/clean_smart/clean_smart_config.htm?version=" + version); //YG 2017-03-26; moved config to azertyfun github hosting
+
   }
 );
 
 Pebble.addEventListener("webviewclosed",
   function(e) {
-    
+
     if (e.response !== '') {
-      
+
       //console.log ("++++ I am inside of 'Pebble.addEventListener(webviewclosed). Resonse from WebView: " + decodeURIComponent(e.response));
-      
+
       //Get JSON dictionary
       var settings = JSON.parse(decodeURIComponent(e.response));
-   
+
       var app_message_json = {};
 
       // preparing app message
@@ -254,20 +239,20 @@ Pebble.addEventListener("webviewclosed",
       app_message_json.KEY_LOCATION_SERVICE = settings.locationService;
       app_message_json.KEY_WEATHER_INTERVAL = settings.weatherInterval;
       app_message_json.KEY_LANGUAGE = settings.language;
-     
+
       // only storing and passing to pebble temperature format if it changed, because it will cause Pebble to reissue weather AJAX
       // (or if forecast.io API Key was set/changed - then we need to update weather as well)
       // (or if coordinates (former woeid) changed - then we need to update weather as well)
       if (current_settings.temperatureFormat != settings.temperatureFormat ||
-          current_settings.forecastIoApiKey != settings.forecastIoApiKey ||
+          current_settings.OWMApiKey != settings.OWMApiKey ||
           current_settings.woeid != settings.woeid) {
         app_message_json.KEY_TEMPERATURE_FORMAT = settings.temperatureFormat;
       }
-      
+
       // storing new settings
       localStorage.setItem('current_settings', JSON.stringify(settings));
       current_settings = settings;
-      
+
       //console.log ("++++ I am inside of 'Pebble.addEventListener(webviewclosed). About to send settings to the phone");
       Pebble.sendAppMessage(app_message_json,
         function(e) {
